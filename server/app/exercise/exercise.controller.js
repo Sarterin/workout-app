abaa56ce1377815ef135d2eb37bf757e698359a4
Sarterin <sarterion137@gmail.com>
2024-02-24 16:38:28 +0300

    Add workout entity and modify exercise methods

--- server/app/exercise/exercise.controller.js
+++ server/app/exercise/exercise.controller.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler'
 import { prisma } from '../prisma.js'
 
 // @desc 		Create new exercise
-// @route 	POST /api/exercise
+// @route 	POST /api/exercises
 // @access 	Private
 export const createNewExercise = asyncHandler(async (req, res) => {
 	const { name, times, iconPath } = req.body
@@ -18,11 +18,58 @@ export const createNewExercise = asyncHandler(async (req, res) => {
 	res.json(exercise)
 })
 
+// @desc 		Update exercise
+// @route 	PUT /api/exercises/:id
+// @access 	Private
+export const updateExercise = asyncHandler(async (req, res) => {
+	const { name, times, iconPath } = req.body
+
+	try {
+		const exercise = await prisma.exercise.update({
+			where: {
+				id: Number(req.params.id)
+			},
+			data: {
+				name,
+				times,
+				iconPath
+			}
+		})
+
+		res.json(exercise)
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found!')
+	}
+})
+
+// @desc 		Delete exercise
+// @route 	DELETE /api/exercises/:id
+// @access 	Private
+export const deleteExercise = asyncHandler(async (req, res) => {
+	try {
+		const exercise = await prisma.exercise.delete({
+			where: {
+				id: Number(req.params.id)
+			}
+		})
+
+		res.json({ message: 'Exercise deleted!' })
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found!')
+	}
+})
+
 // @desc 		Get exercises
 // @route 	Get /api/exercises
 // @access 	Private
 export const getExercises = asyncHandler(async (req, res) => {
-	const exercises = await prisma.exercise.findMany()
+	const exercises = await prisma.exercise.findMany({
+		orderBy: {
+			createdAt: 'desc'
+		}
+	})
 
 	res.json(exercises)
 })
--- server/app/exercise/exercise.routes.js
+++ server/app/exercise/exercise.routes.js
@@ -1,9 +1,18 @@
 import express from 'express'
-import { createNewExercise, getExercises } from './exercise.controller.js'
+import {
+	createNewExercise,
+	deleteExercise,
+	getExercises,
+	updateExercise
+} from './exercise.controller.js'
 import { protect } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, createNewExercise).get(protect, getExercises)
+router
+	.route('/:id')
+	.put(protect, updateExercise)
+	.delete(protect, deleteExercise)
 
 export default router
